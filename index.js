03a5c717c40f7b1df787cf48ef1e024e4efb7a0f
Kevin <kevaschm713@gmail.com>
2021-11-02 22:35:08 -0500

    updating stringify bits again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.setHeader("Content-Type","application/json");
-    res.end(JSON.stringify({ a: 1 }, null, 3));
+    response.end(JSON.stringify({ a: 1}, null, 3));
 });
 
 const port = process.env.PORT || 1337;
