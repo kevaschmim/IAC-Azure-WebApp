42e4039434ca47731fc9021e4a30d408b5ede7f2
Kevin <kevaschm713@gmail.com>
2021-11-02 21:59:40 -0500

    initial for files

---
+++ main.tf
@@ -0,0 +1,63 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  subscription_id = var.arm_subscription_id
+  client_id       = var.arm_principal
+  client_secret   = var.arm_password
+  tenant_id       = var.tenant_id
+  features {}
+}
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup-${random_integer.ri.result}"
+  location = "southcentralus"
+}
+# Create the Linux App Service Plan
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "webapp-asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+# Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
+resource "azurerm_app_service" "webapp" {
+  name                = "webapp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+  
+  source_control {
+    repo_url           = "https://github.com/kevaschmim/nodejs-docs-hello-world"
+    branch             = "master"
+    manual_integration = true
+    use_mercurial      = false
+  }
+}
+
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "web_app_dns" {
+  value = "https://${azurerm_app_service.webapp.default_site_hostname}"
+}
+output "source_control" {
+  value = azurerm_app_service.webapp.source_control
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "arm_subscription_id" {}
+variable "arm_principal" {}
+variable "arm_password" {}
+variable "tenant_id" {}
+#variable "local_default_documents" {
+ # default =  "frontend/index.html"
+#}
