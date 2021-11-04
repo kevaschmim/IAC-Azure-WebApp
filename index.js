c94d0c8d24345c0feaba47813bd329d052d1f795
Kevin <kevaschm713@gmail.com>
2021-11-03 22:50:22 -0500

    updated to add hours

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ let date_time = new Date(ts);
 let date = date_time.getUTCDate();
 let month = date_time.getUTCMonth() + 1;
 let year = date_time.getUTCFullYear();
+let hours = date_time.getUTCHours();
+let mins = date_time.getUTCMinutes()
 
 let fullDate = (year + "-" + month + "-" + date);
-
+let hourTime = ( hours + ":" + mins )
 const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.setHeader("Content-Type","application/json");
-    response.end(JSON.stringify({ today: fullDate}, null, 3));
+    response.end(JSON.stringify({ today: fullDate, time: hourTime}, null, 3));
 });
 
 const port = process.env.PORT || 1337;
